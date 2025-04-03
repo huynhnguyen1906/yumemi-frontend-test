d908c9bb1770a5d8c77932e2af51f6d0b0f69247
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-04-03 11:09:41 +0900

    chore(init): scaffold next app and clean up

---
+++ README.md
@@ -0,0 +1,1 @@
+## Yumemi Frontend Test
---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["100", "300", "400", "500", "600", "700", "800", "900"],
+	display: "swap",
+});
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
